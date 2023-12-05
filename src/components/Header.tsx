40604612508d72048f2910c65695b99faab9ce64
Nikolas Bakalis <Nikolas.Bakalis@acertusdelivers.com>
2023-12-05 11:31:50 -0500

    Header color update.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,9 @@ import { Link } from 'react-router-dom';
 const Header: React.FC = () => {
   return (
     <AppBar position="static" sx={{ 
-        background: 'linear-gradient(45deg, #187aa8 30%, #2196F3 90%)',
-        boxShadow: '0 3px 5px 2px #65808c',
-        color: 'white',
-        backgroundImage: `
-          radial-gradient(circle, transparent 20%, #187aa8 20%),
-          radial-gradient(circle, transparent 20%, #84bed9 20%)`,
-        backgroundSize: '10px 10px',
-        backgroundPosition: '0 0, 5px 5px'
+      background: '#1565C0', // Darker blue color
+      boxShadow: '0 3px 5px 2px #65808c',
+      color: 'white'
       }}>
       <Toolbar>
         <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>
